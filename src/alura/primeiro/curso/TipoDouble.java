3e137b2f36025aa4fd5cb22f62cc49be95e2845c
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-11-27 01:10:07 -0300

    Trabalhando com valores double e conversões !

---
+++ src/alura/primeiro/curso/TipoDouble.java
@@ -0,0 +1,10 @@
+package alura.primeiro.curso;
+
+public class TipoDouble {
+    public static void main(String[] args) {
+
+
+        double salario = 1250.70;
+        System.out.println("Meu salario é: " + salario);
+    }
+}
