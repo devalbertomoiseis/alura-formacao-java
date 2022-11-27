fac06b47947a0678c30f6374de316266864490fd
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-11-27 01:05:47 -0300

    Trabalhando com valores double e conversões !

---
+++ src/alura/primeiro/curso/OutrosTipos.java
@@ -0,0 +1,9 @@
+package alura.primeiro.curso;
+
+public class OutrosTipos {
+    public static void main(String[] args) {
+        double salario = 1280.50;
+        int valor = (int) salario;
+        System.out.println(valor);
+    }
+}
