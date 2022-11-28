905ade4388c3591fb6731bb30c927ccd6d128891
albertomoiseis <contatoalbertomoiseis@gmail.com>
2022-11-28 19:01:50 -0300

    Trabalhando com caracteres e strings!

---
+++ src/alura/primeiro/curso/Caracteres.java
@@ -0,0 +1,19 @@
+package alura.primeiro.curso;
+
+public class Caracteres {
+    public static void main(String[] args) {
+        char letra = 'a';
+        System.out.println(letra);
+        String nome = "Alberto";
+        String sobreNome = "Moiseis";
+        System.out.println(nome + " " + sobreNome);
+
+        char valor = 66;
+        valor = (char) + (valor + 1);
+        System.out.println(valor);
+
+        String palavra = "Palavra";
+
+        System.out.println(palavra + 2022);
+    }
+}
---
+++ src/alura/primeiro/curso/CaracteresExercicio.java
@@ -0,0 +1,11 @@
+package alura.primeiro.curso;
+
+public class CaracteresExercicio {
+    public static void main(String[] args) {
+
+        String parcela1 = "10";
+        String parcela2 = "20";
+
+        System.out.println(parcela1 + parcela2);
+    }
+}
